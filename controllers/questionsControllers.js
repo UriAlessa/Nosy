e514f61eb0c874e72a2c28cdd027650cba393056
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-09-04 12:06:56 -0400

    Route added

---
+++ controllers/questionsControllers.js
@@ -0,0 +1,14 @@
+const Question = require('../models/Question')
+
+const questionsControllers = {
+    getQuestion: async (req, res) => {
+        try {
+            // let question = Question.f
+
+        } catch (e) {
+
+        }
+    }
+}
+
+module.exports = questionsControllers
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
-//crear data
+const express = require("express");
+const router = express.Router();
+const questionsControllers = require("../controllers/questionsControllers");
+// const usersControllers = require("../controllers/usersControllers");
+const passport = require("passport");
+const validator = require("../controllers/validator");
+
+// router.route('/user')
+// .put()
+// .put()
+
+router.route('/question')
+    .get(questionsControllers.getQuestion)
