74d635f939260426237fd17bf270e3897514893d
Rafael Mian <rafaelmian35@gmail.com>
2021-09-04 13:06:40 -0300

    backend mounted

---
+++ models/Question.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const questionSchema = mongoose.Schema({
+  category: { type: String, required: true },
+  question: { type: String, required: true },
+  quesetionImg: { type: String },
+  possibleAnswers: {
+    type: { type: Boolean, default: false },
+    answers: [{ type: String, required: true }],
+  },
+  correctAnswer: { type: String, required: true },
+  diffulty: { type: String, required: true },
+});
+module.exports = mongoose.model("question", questionSchema);
--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
 //crear data
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  avatar: { type: String, required: true },
+  friends: [{ type: mongoose.Types.ObjectId, path: "user" }],
+  fb: { type: Boolean, default: false },
+  google: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("user", userSchema);
