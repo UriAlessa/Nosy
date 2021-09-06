12c2235037b36f85da5ffd63fed32d5706927a1c
Sebastian Campos <bastiampos.uni@gmail.com>
2021-09-06 18:15:57 -0300

    button and terms page

--- models/Question.js
+++ models/Question.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 const questionSchema = mongoose.Schema({
   category: { type: String, required: true },
   question: { type: String, required: true },
-  quesetionImg: { type: String },
-  possibleAnswers: {
-    type: { type: Boolean, default: false },
-    answers: [{ type: String, required: true }],
-  },
+  possibleAnswers: [{ type: String, required: true }],
   correctAnswer: { type: String, required: true },
-  difficulty: { type: String, required: true },
+  creator: { type: mongoose.Types.ObjectId, path: "user" },
+  status: { type: Boolean, default: false }
 });
 module.exports = mongoose.model("question", questionSchema);
