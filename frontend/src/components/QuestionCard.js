4c87d74a0e76d2aade2982a8861a1c37636c2813
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-09-07 18:32:56 -0300

    Game Controller Added

--- frontend/src/components/QuestionCard.js
+++ frontend/src/components/QuestionCard.js
@@ -1,24 +1,39 @@
 import styles from "../styles/questionCard.module.css";
 import { connect } from "react-redux";
 import questionActions from "../redux/actions/questionsActions";
+import { useEffect } from "react";
+import gamesActions from "../redux/actions/gamesActions";
 
 const QuestionCard = (props) => {
   const { question, possibleAnswers, correctAnswer } = props.question;
 
+  useEffect(() => {
+    props.category(null)
+    props.nosy(null)
+  }, [])
+
   let questionAudio = new Audio("/assets/question.wav");
   questionAudio.play();
 
+  let correctAudio = new Audio('/assets/correct.wav')
+  let incorrectAudio = new Audio('/assets/incorrect.wav')
+
+  // token, question, answer, nosy, powers_used, coins_spent
   const clickHandler = (e) => {
+    let answer = correctAnswer === e.target.name
     if (correctAnswer === e.target.name) {
+      correctAudio.play()
       e.target.style.background = "green";
       // props.renderRoulette()
       setTimeout(() => {
         props.setQuestion(null);
       }, 1500);
     } else {
+      incorrectAudio.play()
       e.target.style.background = "red";
       console.log("perdio");
     }
+    props.sendAnswer(props.token, props.question, answer, props.nosy,)
   };
   /* props.AccionDeQuitarUnaVida */
   /* Se le resta una vida */
@@ -72,8 +87,15 @@ const QuestionCard = (props) => {
   );
 };
 
+const mapStateToProps = (state) => {
+  return {
+    token: state.users.token
+  }
+}
+
 const mapDispatchToProps = {
   renderRoulette: questionActions.rouletteRender,
+  sendAnswer: gamesActions.sendAnswer
 };
 
-export default connect(null, mapDispatchToProps)(QuestionCard);
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionCard);
