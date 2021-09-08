8ea642fa21fcfd00e2bf58afe879c1cce0491a8e
Rafael Mian <rafaelmian35@gmail.com>
2021-09-08 11:37:46 -0300

    hosteo actual

--- frontend/src/pages/GameButtons.js
+++ frontend/src/pages/GameButtons.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const GameButtons = (props) => {
   const sendRequest = () => {
     if (props.socket) {
-      props.socket.emit("game_request", "dansep");
+      props.socket.emit("game_request", "rafaelmian35");
     }
   };
 
