82787be1f8eef85963c9b4ceafcc3ce772656f85
Rafael Mian <rafaelmian35@gmail.com>
2021-09-08 13:19:46 -0300

    estupida question

--- frontend/src/components/admin/Questions.js
+++ frontend/src/components/admin/Questions.js
@@ -2,15 +2,10 @@ import styles from "../../styles/questions.module.css";
 import { useEffect, useState } from "react";
 import questionActions from "../../redux/actions/questionsActions";
 import { connect } from "react-redux";
-<<<<<<< HEAD
-
-const Questions = (props) => {
-=======
 import TableData from "./TableData";
 
 const Questions = (props) => {
   // eslint-disable-next-line
->>>>>>> eeaf6aab8a03b1dea51d3bc6980ee7defb6d8713
   const [questions, setQuestions] = useState([]);
 
   const getQuestions = async () => {
@@ -28,10 +23,7 @@ const Questions = (props) => {
 
   useEffect(() => {
     getQuestions();
-<<<<<<< HEAD
-=======
     // eslint-disable-next-line
->>>>>>> eeaf6aab8a03b1dea51d3bc6980ee7defb6d8713
   }, []);
 
   return (
