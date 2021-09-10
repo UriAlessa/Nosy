95703d0e816b6a4260ac6cf8ab9c05271c12cf13
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-09-10 17:52:10 -0300

    Styles added

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -13,11 +13,19 @@ const Dashboard = (props) => {
                 <div className={styles.questionsContainer}>
                 </div>
             </div>
-            <div className={style.loginBox} style={{ height: '90%' }}>
-                <p>Online Users</p>
+            <div className={`${style.loginBox} ${style.connected}`} style={{ height: '90%' }}>
+                <p>Online Users ({users.filter((user) => user.connected === true).length})</p>
                 {users.map((user) => {
-                    if (true) {
-                        return <span>{user.username}</span>
+                    if (user.connected) {
+                        return (
+                            <div className={style.connectedUser}>
+                                <div style={{ backgroundImage: `url('${user.avatar}')` }} className={style.userAvatar}></div>
+                                <div className={style.dataUser}>
+                                    <span>Username: {user.username}</span>
+                                    <span>Coins: {user.coins}</span>
+                                </div>
+                            </div>
+                        )
                     }
                 })}
             </div>
