c8e45ca6f9e0ecb7ad7465e5e6fe185cbf20e6a9
Rafael Mian <rafaelmian35@gmail.com>
2021-09-11 21:24:30 -0300

    X

--- controllers/questionsControllers.js
+++ controllers/questionsControllers.js
@@ -3,23 +3,24 @@ const Question = require("../models/Question");
 const questionsControllers = {
   getQuestion: async (req, res) => {
     const { category, game } = req.params;
-    const questions;
+    let questions;
     if (game.current_player) {
-      questions = game.current_player._id === game.player1.user._id
-      ? game.player1.questions 
-      : game.player2.questions
+      questions =
+        game.current_player._id === game.player1.user._id
+          ? game.player1.questions
+          : game.player2.questions;
     } else {
-      questions = game.player.questions
+      questions = game.player.questions;
     }
     try {
       Question.find({ category })
         .count()
         .exec(async (err, count) => {
           let random = Math.floor(Math.random() * count);
-          let question ;
+          let question;
           do {
             question = await Question.findOne({ category }).skip(random);
-          }while (questions.some(qs=> qs.question._id === question._id))
+          } while (questions.some((qs) => qs.question._id === question._id));
           if (!question) throw new Error();
           res.json({ success: true, response: question });
         });
